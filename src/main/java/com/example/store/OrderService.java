10141acaf415774d178638f2dcecf0e657e67569
esattok <esattokk@gmail.com>
2026-02-04 15:43:24 +0300

    Bean creation logic added

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -1,18 +1,28 @@
 package com.example.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService() {}
-
-    @Autowired
-    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+    public OrderService(PaymentService paymentService) {
+        System.out.println("Order service created");
         this.paymentService = paymentService;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Order service post construct");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Order service pre destroy");
     }
 
     public void placeOrder() {
