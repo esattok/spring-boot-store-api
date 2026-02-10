143e18356ebf877492eb65c5f45a46230e34ed7f
esattok <esattokk@gmail.com>
2026-02-10 15:05:54 +0300

    Wishlist table added

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,28 +1,15 @@
 package com.example.store;
 
-import com.example.store.entities.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-       // ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        User user = User.builder()
-                    .name("admin")
-                   .email("john@example.com")
-                   .password("admin")
-                    .build();
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-
-        Category category = Category.builder().name("category").build();
-        category.addProduct(Product.builder().name("product").price(BigDecimal.valueOf(100.50)).build());
-
-        System.out.println(category);
     }
 
 }
--- src/main/java/com/example/store/entities/Product.java
+++ src/main/java/com/example/store/entities/Product.java
@@ -1,18 +1,17 @@
 package com.example.store.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "products")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Builder
+@Entity
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +21,14 @@ public class Product {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "price")
     private BigDecimal price;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
-    @ToString.Exclude
     private Category category;
+
 }
